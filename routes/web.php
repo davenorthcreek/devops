926dceb0ba2ab93b8ca6c395123f7e00a007a09e
Dave Block <dave@northcreek.ca>
2020-09-30 14:31:21 -0600

    remove direct unauth access to the dashboard iframe

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard-blade-view');
+#Route::view('/', 'dashboard-blade-view');
 
-Route::view('dashboard', 'dashboard-blade-view')->name('dashboard');
+Route::view('dashboard', 'dashboard-blade-view')->name('dashboard')->middleware('auth');
 
 Auth::routes();
 
